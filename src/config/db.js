e89107eb6b1979737b72413d2037ef31c87e20ff
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-03-16 16:12:46 +0530

    Schema done

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose")
+require('dotenv').config();
 
 const Connection = async()=>{
     try {
-        const connect = await mongoose.connect(`${process.env.MONGO_URI}/ideaclan`);
-        console.log(`Connected to the DB ${connect.connection.host}`);
+        const connect = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`Connected to the DB`);
     } catch (error) {
         console.log(`Unable to connect the DB`, error);
     }
 }
 
-module.exports = {ConnectToDB}
+module.exports = {Connection}
